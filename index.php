065fc6938409b296b5dfb23b5626c3568b31c50f
Trang Luu <tluu9@mail.greenriver.edu>
2019-04-08 01:44:48 -0400

    Add title and place-holder

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <title>Cupcakes</title>
+</head>
+<body>
+
+
+<form action ="index.php" method ="get">
+
+
+    <div class="form-row">
+        <div class="form-group col-md-6">
+            <h2>Cupcake Fundraiser</h2>
+            <label for="name">Your name :</label>
+            <input type="text" class="form-control" id="name" placeholder="Please input your name.">
+        </div>
+    </div>
+
+</body>
+</html>
+<?php
