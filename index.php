a4e4e3d48deb8d3fe712c8bae8a0321a8f5472fd
Trang Luu <tluu9@mail.greenriver.edu>
2019-04-08 02:03:36 -0400

    Add checkbox

--- index.php
+++ index.php
@@ -14,11 +14,26 @@
     <div class="form-row">
         <div class="form-group col-md-6">
             <h2>Cupcake Fundraiser</h2>
-            <label for="name">Your name :</label>
+            <p>Your name :</p>
             <input type="text" class="form-control" id="name" placeholder="Please input your name.">
+            <br>
+            <p>Cupcake flavors:</p>
+
+            <input type="checkbox" value = "The Grasshopper" name ="flavor[]"> The Grasshopper<br>
+            <input type="checkbox" value = "Whiskey Maple Bacon" name ="flavor[]"> Whiskey Maple Bacon<br>
+            <input type="checkbox" value = "Carrot Walnut" name ="flavor[]"> Carrot Walnut<br>
+            <input type="checkbox" value = "Salted Caramel Cupcake" name ="flavor[]"> Salted Caramel Cupcake<br>
+            <input type="checkbox" value = "Red Velvet" name ="flavor[]"> Red Velvet<br>
+            <input type="checkbox" value = "Lemon Drop" name ="flavor[]"> Lemon Drop<br>
+            <input type="checkbox" value = "Tiramisu" name ="flavor[]"> Tiramisu<br>
+            <br>
+            <input class="btn btn-success" type="submit" value="Order">
+
+
         </div>
     </div>
-
+</form>
 </body>
 </html>
+
 <?php
