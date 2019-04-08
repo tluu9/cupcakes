1086945fcc32c11577ce349627ee08f03e79d5b7
Trang Luu <tluu9@mail.greenriver.edu>
2019-04-08 03:34:37 -0400

    Add validation

--- index.php
+++ index.php
@@ -1,21 +1,20 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
     <title>Cupcakes</title>
 </head>
 <body>
+<div class="form-row">
+    <div class="form-group col-md-6">
+        <h2>Cupcake Fundraiser</h2>
+        <form action="index.php" method="post">
 
+            <label for="validationCustom03"><p>Your name: <p></p></label>
 
-<form action ="index.php" method ="get">
+            <input type="text" class="form-control" name ="name" id="name" placeholder="Please input your name." required>
 
-
-    <div class="form-row">
-        <div class="form-group col-md-6">
-            <h2>Cupcake Fundraiser</h2>
-            <p>Your name :</p>
-            <input type="text" class="form-control" id="name" placeholder="Please input your name.">
             <br>
             <p>Cupcake flavors:</p>
 
@@ -27,13 +26,34 @@
             <input type="checkbox" value = "Lemon Drop" name ="flavor[]"> Lemon Drop<br>
             <input type="checkbox" value = "Tiramisu" name ="flavor[]"> Tiramisu<br>
             <br>
-            <input class="btn btn-success" type="submit" value="Order">
+            <input class="btn btn-success" type="submit" name="submit" Value="Order"/>
+            <br>
 
+        </form>
+        <?php
 
-        </div>
+        $name = $_POST['name'];
+        if(isset($_POST['submit'])){
+            if(!empty($_POST['flavor'])) {
+                echo "<br>Thank you,  ". $_POST['name']. " ,for your order!<br />";
+                $count = count($_POST['flavor']);
+                $total = $count * 3.50;
+
+                echo "<br>Order Summary: ";
+                foreach($_POST['flavor'] as $selected) {
+                    echo "<p><li>".$selected ."</p>";
+                }
+                echo "Order Total: $".$total." ";
+            }
+            else{
+                echo "<b>Please at least one flavor.</b>";
+            }
+        }
+        ?>
+
     </div>
-</form>
+</div>
+
 </body>
 </html>
 
-<?php
